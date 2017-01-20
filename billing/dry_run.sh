2a9d988ee5a51e0d37437b649cad42e91a70f952
Eric Chen <eriolchan@gmail.com>
2017-01-20 13:40:03 +0800

    update dry_run.sh

--- billing/dry_run.sh
+++ billing/dry_run.sh
@@ -4,13 +4,16 @@ time=$1
 
 for i in $(seq -f "%02g" 0 23)
 do
-    nohup java -jar ImportLogService-1.0-SNAPSHOT.jar -t vos -s ${time}:${i} -e ${time}:${i} 2>&1 &
+    echo "process ${i}"
+    nohup java -jar ImportLogService-1.0-SNAPSHOT.jar -t cdn -s ${time}:${i} -e ${time}:${i} 2>&1 &
 
     number=`ps aux | grep ImportLogService | wc -l`
 
     while [ ${number} -ne 1 ]
-    do 
-        sleep 60
+    do
+        echo "ps num:${number}, sleep:${i}"
+        sleep 10
+        number=`ps aux | grep ImportLogService | wc -l`
     done
 done
 
