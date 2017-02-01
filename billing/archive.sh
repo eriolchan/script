017c771384c5dca7005637cea2f00e51e33adf7b
Eric Chen <eriolchan@gmail.com>
2017-02-01 10:46:21 +0800

    update archive.sh

--- billing/archive.sh
+++ billing/archive.sh
@@ -1,16 +1,17 @@
-#!/bin/bash
+#/bin/bash
 
 function archive()
 {
     folder=$1
-    filename="${folder:0-2}.tar.gz"
+    target=${folder/2/1}
+    filename="${target}/${folder:0-2}.tar.gz"
 
-    echo "begin to archive $folder"
+    echo "begin to archive ${folder}"
 
     cd $folder
-    tar zcf ../$filename *
+    mkdir -p ${target}
+    tar zcf ${filename} *
     rm -r *
-    mv ../$filename .
 }
 
 function get_date_path()
@@ -20,7 +21,7 @@ function get_date_path()
 }
 
 
-basePath="/data/3/billing_archive"
+basePath="/data/2/billing_archive"
 datePath=$(get_date_path)
 
 cdnPath="${basePath}/cdn/${datePath}"
