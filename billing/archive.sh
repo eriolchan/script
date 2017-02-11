742332e1f16cf7a6a4144f788755f04e9cd0f132
Eric Chen <eriolchan@gmail.com>
2017-02-11 22:40:05 +0800

    update archive.sh

--- billing/archive.sh
+++ billing/archive.sh
@@ -1,33 +1,74 @@
 #!/bin/bash
 
-function archive()
-{
-    folder=$1
-    target=${folder/2/1}
-    filename="${target}/${folder:0-2}.tar.gz"
-
-    echo "begin to archive ${folder}"
-
-    cd $folder
-    mkdir -p ${target}
-    tar zcf ${filename} *
-    rm -r *
-}
-
 function get_date_path()
 {
     date=`date -d '2 day ago' +%Y%m%d`
     echo "${date:0:4}/${date:4:2}/${date:0-2}"
 }
 
+function zip()
+{
+    category=$1
+    date=$2
+    from_path="/data/2/billing_archive"
+    to_path="/data/1/billing_archive"
 
-basePath="/data/2/billing_archive"
-datePath=$(get_date_path)
+    folder="${from_path}/${category}/${date}"
+    target="${to_path}/${category}/${date}"
 
-cdnPath="${basePath}/cdn/${datePath}"
-vosPath="${basePath}/vos/${datePath}"
+    echo "start to zip files in folder ${folder}"
+    cd ${folder}
+    mkdir -p ${target}
 
-archive $cdnPath
-archive $vosPath
+    if [ ${category} = "cdn" ]
+    then
+        filename="${target}/${folder:0-2}.tar.gz"
+        echo "zip to ${filename}"
+        tar zcf ${filename} *
+        rm -r *
+    else
+        for name in `ls`
+        do
+            filename="${target}/${name}.tar.gz"
+            echo "zip to ${filename}"
+            tar zcf ${filename} ${name}
+            rm -r ${name}
+            sleep 5s
+        done
+    fi
 
-echo "done!"
+    echo "done!"
+}
+
+function upload()
+{
+    category=$1
+    date=$2
+    from_path="/data/1/billing_archive"
+    to_path="oss://agora-billing"
+
+    folder="${from_path}/${category}/${date}"
+    target="${to_path}/${category}/${date}"
+
+    echo "start to upload files in folder ${folder}"
+    cd ${folder}
+
+    for name in `ls`
+    do
+        filename="${target}/${name}"
+
+        echo "upload to ${filename}"
+        aliyuncli oss MultiUpload ${name} ${filename}
+        sleep 5s
+    done 
+
+    echo "done!"
+}
+
+date_path=$(get_date_path)
+
+zip "cdn" "${date_path}"
+zip "vos" "${date_path}"
+
+#upload "cdn" "${date_path}"
+#upload "vos" "${date_path}"
